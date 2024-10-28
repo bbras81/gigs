2130f6fb6f72086f35b78467d2e6b916bda50b84
Bruno Miguel Brás <miguelazevedodg@gmail.com>
2024-10-28 14:13:39 +0000

    primeiras alterações no addgig

--- gigs/gigs_data.py
+++ gigs/gigs_data.py
@@ -5,15 +5,28 @@ import db
 
 class Gigs:
     def __init__(self):
+
         self.gig_date = ft.TextField(
+            
             label="Gig date",
         )
-        self.gig_hour = ft.Dropdown(label="Gig start", max_menu_height=300)
+        self.hour_picker = ft.Container(
+            content=(
+                ft.CupertinoDatePicker(
+                    date_picker_mode=ft.CupertinoDatePickerMode.TIME,
+                    use_24h_format=True,
+                    minute_interval=15,
+                )
+            ),
+            height=216,
+            padding=ft.padding.only(top=6),
+            visible=False,
+        )
+
         self.gig_cachet = ft.TextField(label="Cachet", suffix_text="€")
         self.gig_local = ft.Dropdown(hint_text="Please select a store")
 
         self.gig_local.options = self.get_house_dropdown()
-        self.gig_hour.options = self.hours_dropdown()
 
     @staticmethod
     def get_house_dropdown():
@@ -22,13 +35,3 @@ class Gigs:
         for client in client_data:
             options.append(ft.dropdown.Option(client[1]))
         return options
-
-    @staticmethod
-    def hours_dropdown():
-        hours = []
-        for i in range(0, 24):
-            if i >= 0 and i < 10:
-                hours.append(ft.dropdown.Option(str(f"0{i}:00h")))
-            else:
-                hours.append(ft.dropdown.Option(str(f"{i}:00h")))
-        return hours
--- views/gigs_form/gigs_add.py
+++ views/gigs_form/gigs_add.py
@@ -1,5 +1,4 @@
 import flet as ft
-
 from gigs.gigs_data import Gigs
 
 
@@ -8,13 +7,27 @@ gigs_info = Gigs()
 
 def gigs_add_c(page: ft.Page, gig_date: str):
     page.title = "Add Gig"
+
+    gigs_info.gig_date.width = page.width / 2
     gigs_info.gig_date.value = gig_date
+
+
+    hour_button = ft.CupertinoFilledButton(
+        content=ft.Text("Hour", color=ft.colors.WHITE),
+        on_click=hour_picker_view,
+    )
+
     return ft.Container(
         content=ft.Column(
             controls=[
                 ft.Text("Add Gig"),
-                gigs_info.gig_date,
-                gigs_info.gig_hour,
+                ft.Row(
+                    controls=[
+                        gigs_info.gig_date,
+                        hour_button,
+                    ]
+                ),
+                gigs_info.hour_picker,
                 gigs_info.gig_local,
                 gigs_info.gig_cachet,
             ],
@@ -22,3 +35,10 @@ def gigs_add_c(page: ft.Page, gig_date: str):
             horizontal_alignment=ft.CrossAxisAlignment.CENTER,
         ),
     )
+
+
+def hour_picker_view(e):
+    if gigs_info.hour_picker:
+        gigs_info.hour_picker.visible = not gigs_info.hour_picker.visible
+        if gigs_info.hour_picker:
+            gigs_info.hour_picker.update()
